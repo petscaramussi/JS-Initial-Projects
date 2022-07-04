9c593b24343293fb327813783ea95e17f641b047
Peterson <44592787+petscaramussi@users.noreply.github.com>
2022-07-04 18:23:36 -0300

    final - project 2

---
+++ 2/app.js
@@ -0,0 +1,38 @@
+// initial count
+let count = 0;
+
+// select value
+const value = document.querySelector("#value");
+
+
+//function color
+function colorVerify(num) {
+    if (num > 0) 
+       return value.style.color = "green";
+    
+    if(num < 0) 
+        return value.style.color = "red";
+   
+    return value.style.color = "hsl(205, 86%, 17%)";
+    
+}
+
+//functions
+function increase() {
+    count++;
+    value.textContent = count;
+    colorVerify(count);
+}
+
+function decrease() {
+    count--;
+    value.textContent = count;
+    colorVerify(count);
+    
+}
+
+function reset() {
+    count = 0;
+    value.textContent = count;
+    colorVerify(count);
+}
