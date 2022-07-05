4674a0083a4253024e11e163ee5df79505394df2
Peterson <44592787+petscaramussi@users.noreply.github.com>
2022-07-05 16:59:57 -0300

    half of project 3

---
+++ 3/app.js
@@ -0,0 +1,42 @@
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const weekdays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  const giveaway = document.querySelector('.giveaway');
+  const deadline = document.querySelector('.deadline');
+  const items = document.querySelector(".deadline-format h4");
+
+  
+  let futureDate = new Date(2022, 7, 5, 11, 30, 0);
+  console.log(futureDate)
+
+  const year = futureDate.getFullYear();
+  const hours = futureDate.getHours();
+  const minutes = futureDate.getMinutes();
+
+  giveaway.textContent = 'giveaway ends on ' + year + " " + hours + ":" + minutes + "am";
+  
+
+
+
+  
