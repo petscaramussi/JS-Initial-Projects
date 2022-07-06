3641424a5d50940bcb07a5e6793c2e9b61c061a6
Peterson <44592787+petscaramussi@users.noreply.github.com>
2022-07-05 21:35:15 -0300

    add date properties

--- 3/app.js
+++ 3/app.js
@@ -22,21 +22,61 @@ const months = [
     "Saturday",
   ];
 
+  const oneDay = 86400000;
+  const oneHour = 3600000;
+  const oneMin = 60000;
+
+
+
+  //get HTML    
   const giveaway = document.querySelector('.giveaway');
   const deadline = document.querySelector('.deadline');
   const items = document.querySelector(".deadline-format h4");
 
   
+  //date init
   let futureDate = new Date(2022, 7, 5, 11, 30, 0);
   console.log(futureDate)
 
+  //date values
   const year = futureDate.getFullYear();
   const hours = futureDate.getHours();
   const minutes = futureDate.getMinutes();
+  const date = futureDate.getDate();
 
-  giveaway.textContent = 'giveaway ends on ' + year + " " + hours + ":" + minutes + "am";
+  let month = futureDate.getMonth();
+  month = months[month];
+  const weekday = weekdays[futureDate.getDay()];
   
 
+
+  //text
+  giveaway.textContent = 'giveaway ends on ' + date + " " + month + " " + year + " " + hours + ":" + minutes + "am";
+
+
+  // future time in ms
+  const futureTime = futureDate.getTime();
+
+
+  function getRemainingTime() {
+    const today = new Date().getTime();
+    const t = futureTime - today;
+    
+    //get days
+    let days = t / oneDay;
+    days = Math.floor(days);
+
+    //get hours
+    let hours = Math.floor ( (t % oneDay / oneHour) );
+    
+    //get minutes
+    
+
+
+
+  }
+
+  getRemainingTime();
 
 
   
